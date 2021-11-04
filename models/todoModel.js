761e7415d4e4f246e315ea05d0dfc2d1ba6b3ff0
Dorel Furdui <dorelfurdui@gmail.com>
2021-11-04 12:15:55 -0400

    Update todoModel.js

--- models/todoModel.js
+++ models/todoModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    userId: { type: String, required: true }
+    userId: { type: String, required: false }
 });
 
 module.exports = Todo = mongoose.model("todo", todoSchema);
